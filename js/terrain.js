0025004b774292eb953f7794bf1db167dced7a64
JCabero <j.c.revolution@hotmail.com>
2016-02-09 00:18:44 +0100

    Añadido js básico

---
+++ js/terrain.js
@@ -0,0 +1,10 @@
+/*
+ * terrain.js
+ * Copyright 2015 Javier Cabero
+ * Licensed under MIT (https://github.com/JavierCabero/terrain/blob/master/LICENSE)
+ */
+
+
+$(document).ready(function() {
+	// ToDo
+});
